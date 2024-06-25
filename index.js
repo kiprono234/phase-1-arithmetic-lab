33cf81cb0ef9a85103c86fed879996e100319676
kevin <kipronok234@gmail.com>
2024-06-25 12:24:38 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1*num2
+multiply
+const random=Math.floor(Math.random() *10) +1 
+random
+const num3 = 9
+const num4 = 5
+const mod =num3%num4
+const max = 20
